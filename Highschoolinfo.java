af89fdaa4f6459a3185a1c3a93d2040e575672e5
SpencerB <74507086+Blunderscout@users.noreply.github.com>
2022-01-06 15:11:32 -0500

    Add files via upload

---
+++ Highschoolinfo.java
@@ -0,0 +1,17 @@
+/*
+ * Name: Spencer Beck
+ * Section: CSC151.0002
+ * Instructor: Joe Parks
+ * Date: January 6, 2022
+ * Description: This program prints out my high school info on three lines using System.out.println
+ */
+public class Highschoolinfo {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to CSC-151 Java Programming!");
+		System.out.println("The high school I attended was my home because I was homeschooled in Tallahassee, FL.");
+		System.out.println("Programming is my future (maybe).");
+
+	}
+
+}
---
+++ Initials.java
@@ -0,0 +1,24 @@
+/*
+ * Name: Spencer Beck
+ * Section: CSC151.0002
+ * Instructor: Joe Parks
+ * Date: January 6, 2022
+ * Description: This program prints out my initials on nine lines using System.out.println
+ */
+public class Initials {
+
+	public static void main(String[] args) {
+		System.out.println("     SSSS TTTTTTTTTT BBBBB   !!");
+		System.out.println("   SS         TT     B   BB  !!");
+		System.out.println(" SS           TT     B    BB !!");
+		System.out.println("   SS         TT     B   BB  !!");
+		System.out.println("     SS       TT     BBBBB   !!");
+		System.out.println("      SS      TT     B   BB  !!");
+		System.out.println("     SS       TT     B    BB !!");
+		System.out.println("   SS         TT     B   BB  !!");
+		System.out.println("SSSS          TT     BBBBB   !!");
+
+
+	}
+
+}
